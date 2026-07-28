3b5f3d3d191d6b76b5f318d5d3600679c680fe75
acbrown98109 <aaron.c.brown2@seattlecolleges.edu>
2026-07-28 04:44:31 -0700

    pages depolyment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/NTSB_Aviation_Research_Explorer/',
   plugins: [react()],
   resolve: {
     alias: {
